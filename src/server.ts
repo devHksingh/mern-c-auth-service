5a76254f29661f79c0bbb03df9e1a727a6450f0c
devHksingh <hksingh.dev@gmail.com>
2024-05-16 18:24:32 +0530

    Integrating Eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-function login(username: string): boolean {
-  return true;
+function login(username: string): string {
+  console.log(username);
+  
+  const user = {
+    name:"test"
+  }
+
+  user.name
+
+  return username;
 }
 
 login("hi");
