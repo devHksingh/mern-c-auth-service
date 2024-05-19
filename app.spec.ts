37add15dd3c3e149986fdcb0ea9b82548c5874b1
devHksingh <hksingh.dev@gmail.com>
2024-05-19 22:15:12 +0530

    add test setup [Jest]

---
+++ app.spec.ts
@@ -0,0 +1,16 @@
+// import { describe, it } from "node:test";
+import request from "supertest";
+import { calculateDiscount } from "./src/utils";
+import app from "./src/app";
+
+describe("App", () => {
+  it("should calculate the discount", () => {
+    const result = calculateDiscount(100, 20);
+    expect(result).toBe(20);
+  });
+
+  it("should return 200 status", async () => {
+    const response = await request(app).get("/").send();
+    expect(response.statusCode).toBe(200);
+  });
+});
