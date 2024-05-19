43f5a4795563c557aefffcae99624e35c4fa81f5
devHksingh <hksingh.dev@gmail.com>
2024-05-19 13:13:40 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
+// import createError from "http-errors";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).send("wlcm to auth service");
 });
 
+//
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+
+  return res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
+});
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
+// import createError from "http-errors";
 import app from "./app";
 import { Config } from "./config/index";
 import logger from "./config/logger";
 
 const startServer = () => {
   const port = Config.PORT;
+
   try {
+    // const testerr = createError(401,"Testing Error handling")
+    // throw testerr
     app.listen(port, () => {
       logger.debug("DEBUB MESSAGE");
       logger.info(`Listening on port ${port}`);
