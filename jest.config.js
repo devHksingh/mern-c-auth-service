3cb0817703fa40acccf717f1feb07e6e14b1d6ae
devHksingh <hksingh.dev@gmail.com>
2024-05-19 22:17:17 +0530

    add jest.cpnfig file

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
 };
 
 // export const preset = 'ts-jest'
